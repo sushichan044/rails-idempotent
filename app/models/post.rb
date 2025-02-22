13781e57f6f0b90dabb957204fcea3d11f75f5a6
sushichan044 <mail@sushichan.live>
2025-02-23 01:18:51 +0900

    fix: do not alias user

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,6 +7,4 @@ class Post < ApplicationRecord
 
   validates :title, presence: true, length: { maximum: 100 }
   validates :content, presence: true, length: { maximum: 1000 }
-
-  alias author user
 end
