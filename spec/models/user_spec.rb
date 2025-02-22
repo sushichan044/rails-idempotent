708123b144cabd4aea8ae1ee9567679e9efeac1f
sushichan044 <mail@sushichan.live>
2025-02-23 00:51:59 +0900

    fix: User.create する

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,13 +2,14 @@
 
 RSpec.describe User, type: :model do
   it 'can be created' do
-    user = User.new(name: 'John Doe')
+    user = User.create(name: 'John Doe')
+
     expect(user).to be_valid
   end
 
   it 'is not created without a name' do
-    user = User.new(name: nil)
-    user.valid?
+    user = User.create(name: nil)
+
     expect(user.errors[:name]).to include("can't be blank")
   end
 end
