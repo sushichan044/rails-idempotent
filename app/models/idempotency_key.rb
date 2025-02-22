2a602b656a809463288af18e1279a2c9d2eed4ed
sushichan044 <mail@sushichan.live>
2025-02-23 02:54:59 +0900

    Idempotency-Key model にロック時間を追加

--- app/models/idempotency_key.rb
+++ app/models/idempotency_key.rb
@@ -3,4 +3,45 @@
 # rbs_inline: enabled
 
 class IdempotencyKey < ApplicationRecord
+  # @rbs () { () -> void } -> void
+  def with_idempotent_lock(&_block)
+    idempotent_lock!
+
+    begin
+      yield
+    ensure
+      idempotent_unlock!
+    end
+  end
+
+  # @rbs () -> bool
+  def expired?
+    expired_at.present? && expired_at < Time.current
+  end
+
+  # @rbs () -> bool
+  def locked?
+    locked_at.present? && locked_at > Time.current
+  end
+
+  private
+
+  # @rbs () -> void
+  def idempotent_lock!
+    with_lock do
+      raise 'Already locked' if locked?
+
+      self.locked_at = Time.current
+    end
+    nil
+  end
+
+  # @rbs () -> void
+  def idempotent_unlock!
+    with_lock do
+      raise 'Not locked' unless locked?
+
+      self.locked_at = nil
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_02_22_172748) do
+ActiveRecord::Schema[8.0].define(version: 2025_02_22_173725) do
   create_table "idempotency_keys", force: :cascade do |t|
     t.string "key", null: false
     t.datetime "expired_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "locked_at"
   end
 
   create_table "posts", force: :cascade do |t|
