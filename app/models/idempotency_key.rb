0b80a7bd38f1d6a38952db2a875e416e5390f770
sushichan044 <mail@sushichan.live>
2025-02-24 00:27:21 +0900

    feat: いい感じに request とマッチできる

--- app/models/idempotency_key.rb
+++ app/models/idempotency_key.rb
@@ -8,19 +8,24 @@ class IdempotencyKey < ApplicationRecord
     class NotLocked < StandardError; end
   end
 
+  EXPIRES_IN = 24.hours #: ActiveSupport::Duration
+
   validates :key, presence: true, uuid: { version: 4 }
-  validates :key, uniqueness: {
-                    scope: %i[request_method request_path],
-                    # Only unique in unexpired
-                    conditions: -> { unexpired }
-                  },
-                  on: :create
+  validate :validate_key_uniqueness_in_resumable
 
   validates :request_method, presence: true, length: { maximum: 10 }
   validates :request_path, presence: true, length: { maximum: 255 }
   validates :request_params, presence: true
 
-  scope :unexpired, -> {}
+  scope :alive, -> { where(updated_at: EXPIRES_IN.ago..) }
+
+  class << self
+    # @rbs (idempotency_key: String, method: String, path: String) -> IdempotencyKey?
+    def find_alive_by_request(idempotency_key:, method:, path:)
+      # 2025.02.24 に実行計画を見た感じでは index_idempotency_keys_on_request_unique_identifier が効いていそう
+      alive.find_by(key: idempotency_key, request_path: path, request_method: method)
+    end
+  end
 
   # @rbs [T] () { () -> T } -> T
   def with_idempotent_lock!
@@ -64,5 +69,15 @@ class IdempotencyKey < ApplicationRecord
 
       update!(locked_at: nil)
     end
+  end
+
+  # () -> void
+  def validate_key_uniqueness_in_resumable
+    return unless resumable?
+
+    # 過去 24 時間以内に作成 / 更新されている場合は、同じ key / method / path のリクエストを新規に受け付けない
+    return unless self.class.where(key: key, request_method: request_method, request_path: request_path).alive.exists?
+
+    errors.add(:key, 'must be unique in alive and not completed')
   end
 end
