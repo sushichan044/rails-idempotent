ae3017c8b1cd3695c2fdebb0c4bf38410a69b4cb
sushichan044 <mail@sushichan.live>
2025-02-23 23:11:01 +0900

    rubocop -A

--- db/migrate/20250223085455_add_request_information_to_idempotency_key.rb
+++ db/migrate/20250223085455_add_request_information_to_idempotency_key.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddRequestInformationToIdempotencyKey < ActiveRecord::Migration[8.0]
   def change
     add_column :idempotency_keys, :request_method, :string, null: false
--- db/migrate/20250223140614_add_response_datas_to_idempotency_key.rb
+++ db/migrate/20250223140614_add_response_datas_to_idempotency_key.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddResponseDatasToIdempotencyKey < ActiveRecord::Migration[8.0]
   def change
     add_column :idempotency_keys, :response_code, :integer
