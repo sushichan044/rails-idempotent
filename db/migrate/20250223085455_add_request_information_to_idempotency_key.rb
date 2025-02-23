4a31d175df71e962d6df6b46a763ee026c3fb65a
sushichan044 <mail@sushichan.live>
2025-02-24 03:07:06 +0900

    chore: rubocop -A

--- db/migrate/20250223085455_add_request_information_to_idempotency_key.rb
+++ db/migrate/20250223085455_add_request_information_to_idempotency_key.rb
@@ -2,8 +2,8 @@
 
 class AddRequestInformationToIdempotencyKey < ActiveRecord::Migration[8.0]
   def change
-    add_column :idempotency_keys, :request_method, :string, null: false
-    add_column :idempotency_keys, :request_path, :string, null: false
+    add_column :idempotency_keys, :request_method, :string, null: false # rubocop:disable Rails/NotNullColumn
+    add_column :idempotency_keys, :request_path, :string, null: false # rubocop:disable Rails/NotNullColumn
     add_column :idempotency_keys, :request_params, :json
   end
 end
