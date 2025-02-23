20a80e20e79fa901711028e4e2e9a6644729df0a
sushichan044 <mail@sushichan.live>
2025-02-23 21:57:26 +0900

    refactor: only parse uuid string once

--- app/validators/uuid_validator.rb
+++ app/validators/uuid_validator.rb
@@ -6,27 +6,22 @@ class UuidValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value) # rubocop:disable Metrics/ AbcSize
     return if value.nil?
 
-    record.errors.add(attribute, options[:message] || 'is not a valid UUID') unless uuid?(value)
+    begin
+      uuid = UUIDTools::UUID.parse(value)
+    rescue ArgumentError, TypeError
+      record.errors.add(attribute, options[:message] || 'is not a valid UUID')
+      return
+    end
 
-    return unless options[:version] && !uuid_version?(value, options[:version])
+    return unless options[:version] && !uuid_version?(uuid, options[:version])
 
     record.errors.add(attribute, options[:message] || "is not a valid UUID version #{options[:version]}")
   end
 
   private
 
-  # @rbs (String) -> bool
-  def uuid?(string)
-    string.match?(UUIDTools::UUID_REGEXP)
-  end
-
-  # @rbs (String, Integer) -> bool
-  def uuid_version?(string, version)
-    begin
-      parsed = UUIDTools::UUID.parse(string)
-    rescue TypeError, ArgumentError
-      return false
-    end
-    parsed.version == version
+  # @rbs (UUIDTools::UUID, Integer) -> bool
+  def uuid_version?(uuid, version)
+    uuid.version == version
   end
 end
