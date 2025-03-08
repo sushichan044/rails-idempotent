27c33325b18628984e1d8b0d2de5693c97191b84
sushichan044 <mail@sushichan.live>
2025-03-08 22:29:03 +0900

    fix: add global error handler

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,12 @@
 # rbs_inline: enabled
 
 class ApplicationController < ActionController::API
+  rescue_from StandardError do |exception|
+    unless Rails.env.development?
+      render json: {data: nil, error: "Internal server error"}, status: :internal_server_error
+      break
+    end
+
+    render json: {data: nil, error: exception.message}, status: :internal_server_error
+  end
 end
